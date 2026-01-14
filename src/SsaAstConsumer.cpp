d3040cebbe1938f045c82c15eea7e62317891575
NazimKYS <salimyahia.kissi@univ-tlemcen.dz>
2026-01-14 12:13:37 +0100

    disable printing much details regarding ssa stmt

--- src/SsaAstConsumer.cpp
+++ src/SsaAstConsumer.cpp
@@ -17,15 +17,16 @@ public:
     void HandleTranslationUnit(ASTContext& context) override {
         visitor.TraverseDecl(context.getTranslationUnitDecl());
 
-        // Output example
-         llvm::outs() << "\n=== SSA Definitions by Line ===\n";
+        // displaying  SSA log like : Line 33: serviceId_3 = 15 (stmtID=2633992568)
+        /*
+        llvm::outs() << "\n=== SSA Definitions by Line ===\n";
         for (const auto& [line, defs] : visitor.getLineMap()) {
             for (const auto& def : defs) {
                 llvm::outs() << "Line " << line << ": " << def.exprString
                              << " (stmtID=" << def.stmtID << ")\n";
             }
         }
-
+        */
         llvm::outs() << "\n=== SSA Variable Version History ===\n";
         visitor.printVariableHistory();
     }
